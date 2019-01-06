9abb0fe08a34d7550eed27ac4a5706e3e2f128f0
Louis <louis.foged@gmail.com>
2019-01-06 14:34:12 +0100

    refactors code into more descriptive modules, restuctures & moves control for quote response into 'INIT' module

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 'use strict';
 
-/* FUNCTIONS & VARIABLES USED THROUGHOUT APP */
-const GLOBAL = (() => {
-  const doc = document;
-  const els = Object.freeze({
-    alertSection: doc.querySelector('.alert-section'),
-    displaySection: doc.querySelector('.display-section'),
-    favSection: doc.querySelector('.favorites-section'),
-    quoteButtons: doc.querySelector('.quote-buttons'),
-    tweetLink: doc.querySelector('#tweet-link'),
-    wikiLink: doc.querySelector('#wiki-link'),
-    newFavBtn: doc.querySelector('#new-fav'),
-    clearFavBtn: doc.querySelector('#clear-all-fav')
-  });
 
+/* UTILITY FUNCTIONS */
+const UTILS = (() => {
+  const _doc = document;
+
+  // create elements, assign class & attributes if any
   const createEls = (elArr = []) => {
     const els = elArr.map((elObj) => {
-      const el = doc.createElement(elObj.el);
+      const el = _doc.createElement(elObj.el);
       el.className = elObj.class;
       if (elObj.attr) el[elObj.attr.name] = elObj.attr.val;
       
@@ -26,93 +18,126 @@ const GLOBAL = (() => {
     return els;
   };
 
+  // remove element after x millisecond delay - for alert
   const removeEl = (elQuerySelector, delay) => {
     return setTimeout(() => {
-      doc.querySelector(elQuerySelector).remove();
+      _doc.querySelector(elQuerySelector).remove();
     }, delay);
   };
 
-  const getElAttr = (elQuerySelector, attr) => {
-    return doc.querySelector(elQuerySelector)[attr]
-      || alertCtrl('error', 'Element or Attribute Not Found');
-  };
-
-  // recursively remove HTML childNodes from element
+  // recursively remove HTML childNodes from an element
   const removeKids = (element) => {
     if (element.hasChildNodes()) {
       element.removeChild(element.firstChild);
   
-      return removeKids(element);
+      return UTILS.removeKids(element);
     }
   };
-  
-  // returns function for creating specified template 
-  const makeTemplate = (templateName) => {
-    const templates = Object.freeze({
-      alert: (alertType, msg = 'Oh fudge! Something Broke') => {
-        const [alertDiv, alertMsg] = createEls([
-          {el: 'div', class: `alert ${alertType}`},
-          {el: 'p', class: 'alert-msg', attr: {name: 'textContent', val: msg}}
-        ]);
-        alertDiv.appendChild(alertMsg);
-    
-        return alertDiv;
-      },
-      jsonP: (url) => {
-        return createEls([{
-          el: 'script',
-          class: 'json-p',
-          attr: {name: 'src', val: url}
-        }]);
-      },
-      quote: (quoteObj) => {
-        return createEls([
-          {
-            el: 'h1',
-            class: 'display-quote',
-            attr: {name: 'textContent', val: quoteObj.quote}
-          },
-          {
-            el: 'h3',
-            class: 'display-author',
-            attr: {name: 'textContent', val: `- ${quoteObj.author}`}
-          }
-        ]);
-      },
-      favorite: (quote, index) => {
-        const [favDiv, favText, favAuthor, favRemove] = createEls([
-          {el: 'div', class: `fav ${index}`},
-          {
-            el: 'p',
-            class: 'fav-text',
-            attr: {name: 'textContent', val: `${quote.quote}`}
-          },
-          {
-            el: 'span',
-            class: 'fav-author',
-            attr: {name: 'textContent', val: `- ${quote.author}`}
-          },
-          {
-            el: 'button',
-            class: 'btn fav-remove',
-            attr: {name: 'textContent', val: 'DELETE'}
-          }
-        ]);
-        favText.appendChild(favAuthor);
-        favDiv.appendChild(favText);
-        favDiv.appendChild(favRemove);
 
-        return favDiv;
-      }
-    });
-
-    return templates[templateName];
+  // return specified attribute of an element
+  const getElAttr = (elQuerySelector, attr) => {
+    return _doc.querySelector(elQuerySelector)[attr]
+      || alertCtrl('error', 'Element or Attribute Not Found');
   };
+
+  // appends Array of elements to a document fragment
+  const appendToFragment = (elArr = []) => {
+    const fragment = _doc.createDocumentFragment();
+    elArr.map((el) => fragment.appendChild(el));
+
+    return fragment;
+  };
+
+  return Object.freeze({
+    createEls,
+    removeEl,
+    removeKids,
+    getElAttr,
+    appendToFragment
+  });
+})();
+
+
+/* FUNCTIONS & VARIABLES THAT DIRECTLY DEAL WITH / 'TOUCH' DOM */
+const DOM = ((UTILS) => {
+  const {createEls, appendToFragment, removeEl} = UTILS;
+  const _doc = document;
+
+  // DOM elements
+  const els = Object.freeze({
+    alertSection: _doc.querySelector('.alert-section'),
+    displaySection: _doc.querySelector('.display-section'),
+    favSection: _doc.querySelector('.favorites-section'),
+    quoteButtons: _doc.querySelector('.quote-buttons'),
+    tweetLink: _doc.querySelector('#tweet-link'),
+    wikiLink: _doc.querySelector('#wiki-link'),
+    newFavBtn: _doc.querySelector('#new-fav'),
+    clearFavBtn: _doc.querySelector('#clear-all-fav')
+  });
+  
+  // templates for alert, quotes, etc.
+  const templates = Object.freeze({
+    alert: (alertType, msg = 'Oh fudge! Something Broke') => {
+      const [alertDiv, alertMsg] = createEls([
+        {el: 'div', class: `alert ${alertType}`},
+        {el: 'p', class: 'alert-msg', attr: {name: 'textContent', val: msg}}
+      ]);
+      alertDiv.appendChild(alertMsg);
+  
+      return alertDiv;
+    },
+    jsonP: (url) => {
+      return createEls([{
+        el: 'script',
+        class: 'json-p',
+        attr: {name: 'src', val: url}
+      }]);
+    },
+    quote: (quoteObj) => {
+      return createEls([
+        {
+          el: 'h1',
+          class: 'display-quote',
+          attr: {name: 'textContent', val: quoteObj.quote}
+        },
+        {
+          el: 'h3',
+          class: 'display-author',
+          attr: {name: 'textContent', val: `- ${quoteObj.author}`}
+        }
+      ]);
+    },
+    favorite: (quote, index) => {
+      const [favDiv, favText, favAuthor, favRemove] = createEls([
+        {el: 'div', class: `fav ${index}`},
+        {
+          el: 'p',
+          class: 'fav-text',
+          attr: {name: 'textContent', val: `${quote.quote}`}
+        },
+        {
+          el: 'span',
+          class: 'fav-author',
+          attr: {name: 'textContent', val: `- ${quote.author}`}
+        },
+        {
+          el: 'button',
+          class: 'btn fav-remove',
+          attr: {name: 'textContent', val: 'DELETE'}
+        }
+      ]);
+      favText.appendChild(favAuthor);
+      favDiv.appendChild(favText);
+      favDiv.appendChild(favRemove);
+
+      return favDiv;
+    }
+  });
 
   // if no 'alert' <div> in DOM, create one, append it & remove after x secs
   const alertCtrl = (alertType, msg = 'Oh Fudge! Something Broke') => {
-    if (!doc.querySelector('.alert')) {
-      const template = makeTemplate('alert')(alertType, msg);
+    if (!_doc.querySelector('.alert')) {
+      const template = templates.alert(alertType, msg);
       const fragment = appendToFragment([template]);
       printer(els.alertSection, fragment);
       
@@ -120,67 +145,22 @@ const GLOBAL = (() => {
     }
   };
 
-  const appendToFragment = (elArr = []) => {
-    const fragment = doc.createDocumentFragment();
-    elArr.map((el) => fragment.appendChild(el));
-
-    return fragment;
-  };
-
+  // 'prints' (appends) fragment to DOM
   const printer = (target, fragment) => target.appendChild(fragment);
 
-  return Object.freeze({
-    els,
-    getElAttr,
-    removeKids,
-    makeTemplate,
-    appendToFragment,
-    alertCtrl,
-    printer
-  });
-})();
+  return Object.freeze({ els, templates, alertCtrl, printer });
+})(UTILS);
 
 
 /* FUNCTIONS FOR GETTING & PROCESSING QUOTES */
-const quoteModule = ((GLOBAL) => {
-  const {displaySection, wikiLink, tweetLink} = GLOBAL.els;
-  const {
-    alertCtrl,
-    makeTemplate,
-    appendToFragment,
-    removeKids,
-    printer
-  } = GLOBAL;
+const QUOTES = ((UTILS, DOM) => {
+  const {removeKids, appendToFragment} = UTILS;
+  const {els, templates, alertCtrl, printer} = DOM;
+  const {displaySection, wikiLink, tweetLink} = els;
 
-  // set URL for request, determine type of request to make and send request
-  const prepReq = (quoteType) => {
-    const apiUrls = {
-      random: 'https://favqs.com/api/qotd',
-      inspire: `https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=quoteModule.quoteCtrl`,
-      got: 'https://got-quotes.herokuapp.com/quotes'
-    };
-    const url = apiUrls[quoteType];
-    const useJsonP = ['inspire'];
 
-    return (useJsonP.includes(quoteType)) ? jsonPReq(url) : fetchReq(url); 
-  };
-
-  const fetchReq = (url) => {
-    return fetch(url)
-      .then((res) => res.json())
-      .then(quoteCtrl)
-      .catch((err) => alertCtrl('error', 'Request Error'));
-  };
-
-  const jsonPReq = (url) => {
-    const [jsonPScriptEl] = makeTemplate('jsonP')(url);
-    const docBody = document.body;
-    docBody.appendChild(jsonPScriptEl);
-
-    return docBody.lastChild.remove();
-  };
-
-  const formatQuote = (quote) => {
+  // format raw response data (quote)
+  const _formatQuote = (quote) => {
     return {
       quote: quote.quoteText || quote.quote.body || quote.quote,
       author: ((quote.quoteAuthor === '') ? 'Unknown' : quote.quoteAuthor)
@@ -189,7 +169,8 @@ const quoteModule = ((GLOBAL) => {
     };
   };
 
-  const setWikiTweetLinks = (quoteObj) => {
+  // manage wiki & tweet link buttons according to response quote
+  const _setWikiTweetLinks = (quoteObj) => {
     const author = quoteObj.author.toLowerCase();
     if (author === 'unknown' || author === 'anonymous') {
       wikiLink.style.display = 'none';
@@ -206,32 +187,44 @@ const quoteModule = ((GLOBAL) => {
       }`;
   };
 
-  // ctrl flow after response => NOTE: public function so jsonP res can find it
-  const quoteCtrl = (rawResponse) => {
-    const formattedQuote = formatQuote(rawResponse);
-    const template = makeTemplate('quote')(formattedQuote);
-    const fragment = appendToFragment(template);
-    removeKids(displaySection);
-    setWikiTweetLinks(formattedQuote)
-    
-    return printer(displaySection, fragment);
+  // set URL, determine type of request (fetch / JSONP) & make request
+  const makeReq = (quoteType, handler) => {
+    const apiUrls = {
+      random: 'https://favqs.com/api/qotd',
+      inspire: `https://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&lang=en&jsonp=INIT.quoteResponseCtrl`,
+      got: 'https://got-quotes.herokuapp.com/quotes'
+    };
+    const url = apiUrls[quoteType];
+    const useJsonP = ['inspire'];
+
+    // make fetch or JSONP request => response handled by 'quoteResponseCtrl'
+    return (useJsonP.includes(quoteType)) ? jsonPReq(url) : fetchReq(url, handler); 
   };
 
-  return Object.freeze({prepReq, quoteCtrl});
-})(GLOBAL);
+  const fetchReq = (url, handler) => {
+    return fetch(url)
+      .then((res) => res.json())
+      .then(handler)
+      .catch((err) => alertCtrl('error', 'Request Error'));
+  };
+
+  const jsonPReq = (url) => {
+    const [jsonPScriptEl] = templates.jsonP(url);
+    const docBody = document.body;
+    docBody.appendChild(jsonPScriptEl);
+
+    return docBody.lastChild.remove();
+  };
+
+  return Object.freeze({ makeReq, _formatQuote, _setWikiTweetLinks });
+})(UTILS, DOM);
 
 
 /* FUNCTIONS FOR MANAGING DATA SAVED TO localStorage */
-const favoriteModule = ((GLOBAL) => {
-  const {favSection, clearFavBtn} = GLOBAL.els;
-  const {
-    alertCtrl,
-    getElAttr,
-    makeTemplate,
-    removeKids,
-    appendToFragment,
-    printer
-  } = GLOBAL;
+const FAVORITES = ((DOM, UTILS) => {
+  const {els, templates, alertCtrl, printer} = DOM;
+  const {favSection, clearFavBtn} = els;
+  const {getElAttr, removeKids, appendToFragment} = UTILS
   const store = localStorage;
 
   const getQuotes = () => JSON.parse(store.getItem('quotes'));
@@ -243,8 +236,8 @@ const favoriteModule = ((GLOBAL) => {
     removeKids(favSection);
     if (!store.length) return clearFavBtn.hidden = true;
     const quotes = getQuotes();
-    const TEMPLATESArr = quotes.map(makeTemplate('favorite'));
-    const fragment = appendToFragment(TEMPLATESArr);
+    const templatesArr = quotes.map(templates.favorite);
+    const fragment = appendToFragment(templatesArr);
     clearFavBtn.removeAttribute('hidden');
   
     return printer(favSection, fragment);
@@ -297,30 +290,46 @@ const favoriteModule = ((GLOBAL) => {
   };
 
   return Object.freeze({printAll, addOne, removeOne, removeAll});
-})(GLOBAL);
+})(DOM, UTILS);
 
 
 // initialize program
-const init = ((els, alertCtrl, prepReq, favoriteModule) => {
-  const {quoteButtons, newFavBtn, clearFavBtn, favSection} = els;
-  const {printAll, addOne, removeOne, removeAll} = favoriteModule;
+const INIT = ((DOM, QUOTES, FAVORITES) => {
+  const {els, alertCtrl, templates, printer} = DOM;
+  const {displaySection, quoteButtons, newFavBtn, clearFavBtn, favSection} = els;
+  const {makeReq, _formatQuote, _setWikiTweetLinks} = QUOTES;
+  const {printAll, addOne, removeOne, removeAll} = FAVORITES;
+
+
+
+  // handler after response => NOTE: public function so jsonP res can find it
+  const quoteResponseCtrl = (rawResponse) => {
+    const formattedQuote = _formatQuote(rawResponse);
+    const template = templates.quote(formattedQuote);
+    const fragment = UTILS.appendToFragment(template);
+    UTILS.removeKids(displaySection);
+    _setWikiTweetLinks(formattedQuote)
+    
+    return printer(displaySection, fragment);
+  };
+
 
   // fetch 'random' quote on load & print any saved favorites
-  prepReq('random');
+  makeReq('random', quoteResponseCtrl);
   if (localStorage.length) printAll();
 
-  // print an error msg to screen if no quote 2sec after load
+  // print an error msg to screen if no quote 2.5sec after load
   setTimeout(() => {
     if (!document.querySelector('.display-quote')) {
       const errMsg = 'There might be a request error. Please try again'
 
       return alertCtrl('error', errMsg);
     }
-  }, 2000);
+  }, 2500);
 
   /* event listeners */
   quoteButtons.addEventListener('click', (e) => {
-    if (e.target.tagName === 'BUTTON') return prepReq(e.target.id);
+    if (e.target.tagName === 'BUTTON') return makeReq(e.target.id, quoteResponseCtrl);
   });  
   newFavBtn.addEventListener('click', addOne);
   clearFavBtn.addEventListener('click', removeAll);
@@ -329,4 +338,6 @@ const init = ((els, alertCtrl, prepReq, favoriteModule) => {
       return removeOne(e.target);
     }
   });
-})(GLOBAL.els, GLOBAL.alertCtrl, quoteModule.prepReq, favoriteModule);
+
+  return Object.freeze({ quoteResponseCtrl });
+})(DOM, QUOTES, FAVORITES);
